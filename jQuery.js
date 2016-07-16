1818fe60f140b913200fd033e4da7864da904009
llwang8 <wangxbl56@gmail.com>
2016-07-16 13:20:55 -0400

    add js to interactively change break length, session length and timer session length

--- jQuery.js
+++ jQuery.js
@@ -1,1 +1,32 @@
 // freecodecamp - Pomodoro Clock -->
+
+$(document).ready(function(){
+    //change break length
+    var setting = '';
+    $('.break').click(function(){
+        setting = $('.break-setting').text()
+        $('.break-setting').text(updateSetting(setting, $(this).text()));
+
+    });
+
+    //change session length and update clock's session length
+    $('.session').click(function(){
+        setting = $('.session-setting').text()
+        var newSession = updateSetting(setting, $(this).text());
+        $('.session-setting').text(newSession);
+        $('.timer').text(newSession);
+
+    });
+
+    function updateSetting(setting, operation){
+        if (operation === "+"){
+            return eval( setting + '+ 1');
+        }else {
+            return eval( setting + '- 1');
+        }
+    }
+
+});
+
+
+
