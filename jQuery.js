1014e646f4e6b45e4950a9b8f8f6ef49f912a15d
llwang8 <wangxbl56@gmail.com>
2016-07-16 22:21:20 -0400

    add timer countdown js

--- jQuery.js
+++ jQuery.js
@@ -26,7 +26,77 @@ $(document).ready(function(){
         }
     }
 
+    //canvas circle
+    var canvas = document.getElementById("canvas");
+    var ctx = canvas.getContext("2d");
+    var radius = canvas.height / 2;
+    ctx.translate(radius, radius);
+    radius = radius * 0.90
+    drawClock();
+
+    function drawClock() {
+        ctx.arc(0, 0, radius, 0 , 2*Math.PI);
+        ctx.fillStyle = "white";
+        ctx.fill();
+    }
+
+    //display timer countdown
+    var endtime = Date.parse(new Date()) + eval($('.timer').text()) * 60 * 1000;
+    //alert(endtime);
+    updateClock();
+    var timeinterval = setInterval(updateClock, 1000);
+
+    function getTimeRemaining(endtime){
+        var t = Date.parse(endtime) - Date.parse(new Date());
+        var seconds = Math.floor((t / 1000) % 60);
+        var minutes = Math.floor((t / 1000 / 60) % 60);
+
+        return {
+            'total': t,
+            'minutes': minutes,
+            'seconds': seconds
+        }
+    }
+
+    function updateClock(){
+        var t = getTimeRemaining(endtime);
+        $('.countdown').text(t.minutes + ":" + t.seconds);
+        if (t.total <= 0){
+            clearInterval(timeinterval);
+        }
+    }
+
+
+
 });
 
 
+//test timer countdown
+var endtime = Date.parse(new Date()) + eval('3') * 60 * 1000;
+
+var t = Date.parse(endtime) - Date.parse(new Date());
+var seconds = Math.floor((t / 1000) % 60);
+var minutes = Math.floor((t / 1000 / 60) % 60);
+updateClock();
+var timeinterval = setInterval(updateClock, 1000);
+
+    function getTimeRemaining(endtime){
+        var t = Date.parse(endtime) - Date.parse(new Date());
+        var seconds = Math.floor((t / 1000) % 60);
+        var minutes = Math.floor((t / 1000 / 60) % 60);
+
+        return {
+            'total': t,
+            'minutes': minutes,
+            'seconds': seconds
+        }
+    }
+
+    function updateClock(){
+        var t = getTimeRemaining(endtime);
+        console.log(t.minutes + ":" + t.seconds);
+        if (t.total <= 0){
+            clearInterval(timeinterval);
+        }
+    }
 
